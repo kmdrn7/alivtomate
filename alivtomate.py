884a0fd5234122e40e211aa9b115ab5ad5678ca2
Andika Ahmad Ramadhan <andikahmadr@gmail.com>
2020-10-02 07:48:25 +0700

    nambahin waktu sleep

--- alivtomate.py
+++ alivtomate.py
@@ -200,7 +200,7 @@ class Alivtomate(unittest.TestCase):
 
             submitButton = driver.find_element_by_name('next')
             submitButton.click()
-            time.sleep(15)
+            time.sleep(20)
 
     def test_DailyDictation(self):
         self.loginCasMis()
@@ -255,7 +255,7 @@ class Alivtomate(unittest.TestCase):
 
             finishButton = driver.find_element_by_name('next')
             finishButton.click()
-            time.sleep(15)
+            time.sleep(20)
 
     def tearDown(self):
         self.driver.close()
